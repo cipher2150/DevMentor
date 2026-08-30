fd56f503d0bf6e5e8710432f2002bbb3aa5c9f9f
Rajshekhar Prasad Saxena <121630495+cipher2150@users.noreply.github.com>
2026-08-30 16:33:07 +0530

    base url is changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = '/api';
+const API_BASE = 'https://devmentor-nvis.onrender.com/api';
 
 export async function analyzeRepo(repoUrl) {
   const response = await fetch(`${API_BASE}/analyze`, {
